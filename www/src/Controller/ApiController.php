fc6ee19a99c6fb155a43a8772986e7804036f8c5
Liviu Balan <liv_romania@yahoo.com>
2021-08-31 17:13:24 +0300

    Check PHP $_SERVER info

--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 class ApiController extends AbstractController {
     public function test()
     {
+        var_dump($_SERVER);
+        die;
         $bitcoin = new Blockchain();
 
         return new JsonResponse($bitcoin);
