57c0282ff8bed0ba554b6b28834d0433da9385ad
Liviu Balan <liv_romania@yahoo.com>
2021-08-31 17:01:49 +0300

    Create Genesis block
    URL:
    https://en.bitcoin.it/wiki/Genesis_block

--- www/src/Classes/Blockchain.php
+++ www/src/Classes/Blockchain.php
@@ -7,6 +7,11 @@ class Blockchain
     public array $chain = [];
     public array $pendingTransactions = [];
 
+    public function __construct()
+    {
+        $this->createNewBlock(0, '0', '0'); // Genesis block
+    }
+
     public function createNewBlock(int $nonce, string $previousBlockHash, string $hash): array
     {
         $newBlock = [
--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -11,30 +11,6 @@ class ApiController extends AbstractController {
     {
         $bitcoin = new Blockchain();
 
-        $previousBlockHash = 'hash-1';
-        $currentBlockData = [
-            [
-                'amount' => 10,
-                'sender' => 'sender-1',
-                'recipient' => 'recipient-1',
-            ],
-            [
-                'amount' => 30,
-                'sender' => 'sender-2',
-                'recipient' => 'recipient-2',
-            ],
-            [
-                'amount' => 200,
-                'sender' => 'sender-3',
-                'recipient' => 'recipient-3',
-            ],
-        ];
-
-        $nonce = $bitcoin->proofOfWork($previousBlockHash, $currentBlockData); // number of iterations to find the nonce
-        var_dump($nonce);
-        var_dump($bitcoin->hashBlock($previousBlockHash, $currentBlockData, $nonce)); // verify the result
-        die;
-
         return new JsonResponse($bitcoin);
     }
 }
