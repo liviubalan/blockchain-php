5f0c6755fe2076a27bc09f0e970cf91acdf925b8
Liviu Balan <liv_romania@yahoo.com>
2021-08-31 16:17:02 +0300

    Blockchain usage: method "createNewBlock"

--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -10,18 +10,12 @@ class ApiController extends AbstractController {
     public function test()
     {
         $bitcoin = new Blockchain();
+
         $bitcoin->createNewBlock(1, 'hash-0', 'hash-1');
-        $bitcoin->createNewBlock(2, 'hash-1', 'hash-2');
-        $bitcoin->createNewBlock(3, 'hash-2', 'hash-3');
+//        $bitcoin->createNewTransaction(100, 'sender-1', 'recipient-1');
+//        $bitcoin->createNewBlock(2, 'hash-1', 'hash-2');
+//        $bitcoin->createNewBlock(3, 'hash-2', 'hash-3');
 
-        $message = 'test';
-        echo 'md5: '.md5($message)."\n";
-        echo 'sha1: '.sha1($message)."\n";
-        echo 'crc32: '.crc32($message)."\n";
-        echo 'sha256: '.hash('sha256', $message)."\n";
-        var_dump($message);
-        exit;
-
-        return new JsonResponse($bitcoin->getLastBlock());
+        return new JsonResponse($bitcoin);
     }
 }
