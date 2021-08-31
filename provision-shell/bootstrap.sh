f76c069abe323a7e2852d06c1dd11d16fbfcdb38
Liviu Balan <liv_romania@yahoo.com>
2021-08-31 11:01:23 +0300

    Install Nginx
    URL:
    https://www.digitalocean.com/community/tutorials/how-to-install-linux-nginx-mysql-php-lemp-stack-on-centos-7

--- provision-shell/bootstrap.sh
+++ provision-shell/bootstrap.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+# Install Git
 sudo yum -y install git
+
+# Add the CentOS 7 EPEL repository
+sudo yum -y install epel-release
+
+# Install Nginx
+sudo yum -y install nginx
+
+# Start the Nginx service
+sudo systemctl start nginx
+
+# Enable Nginx to start on boot
+sudo systemctl enable nginx
