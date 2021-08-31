468f38b6b9814a9d04c597176cab0f7edcb64a00
Liviu Balan <liv_romania@yahoo.com>
2021-08-31 16:22:24 +0300

    Blockchain usage: "createNewTransaction" + "createNewBlock"

--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -12,9 +12,14 @@ class ApiController extends AbstractController {
         $bitcoin = new Blockchain();
 
         $bitcoin->createNewBlock(1, 'hash-0', 'hash-1');
+
         $bitcoin->createNewTransaction(100, 'sender-1', 'recipient-1');
-        $bitcoin->createNewBlock(2, 'hash-1', 'hash-2');
-//        $bitcoin->createNewBlock(3, 'hash-2', 'hash-3');
+        $bitcoin->createNewBlock(2, 'hash-1', 'hash-2'); // mine a new block
+
+        $bitcoin->createNewTransaction(50, 'sender-2', 'recipient-2');
+        $bitcoin->createNewTransaction(300, 'sender-3', 'recipient-3');
+        $bitcoin->createNewTransaction(2000, 'sender-4', 'recipient-4');
+//        $bitcoin->createNewBlock(3, 'hash-2', 'hash-3'); // mine a new block
 
         return new JsonResponse($bitcoin);
     }
