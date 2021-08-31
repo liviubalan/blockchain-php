e2f4de1763e7791f3aca600c34acf4ddd2789cf4
Liviu Balan <liv_romania@yahoo.com>
2021-08-31 16:10:48 +0300

    Test PHP hashing functions
    URL:
    https://en.wikipedia.org/wiki/Cryptographic_nonce
    https://en.wikipedia.org/wiki/Cryptographic_hash_function
    https://www.php.net/manual/en/function.md5.php
    https://www.php.net/manual/en/function.sha1.php
    https://www.php.net/manual/en/function.crc32.php
    https://www.php.net/manual/en/function.hash.php
    https://www.php.net/manual/en/function.var-dump.php

--- www/src/Controller/ApiController.php
+++ www/src/Controller/ApiController.php
@@ -14,6 +14,14 @@ class ApiController extends AbstractController {
         $bitcoin->createNewBlock(2, 'hash-1', 'hash-2');
         $bitcoin->createNewBlock(3, 'hash-2', 'hash-3');
 
+        $message = 'test';
+        echo 'md5: '.md5($message)."\n";
+        echo 'sha1: '.sha1($message)."\n";
+        echo 'crc32: '.crc32($message)."\n";
+        echo 'sha256: '.hash('sha256', $message)."\n";
+        var_dump($message);
+        exit;
+
         return new JsonResponse($bitcoin->getLastBlock());
     }
 }
